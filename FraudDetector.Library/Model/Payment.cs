68723ea97c7949724c093bacf4a12af762e210cd
Mateusz <mateusz.wolnica@gmail.com>
2025-08-08 19:55:09 +0200

    .

--- FraudDetector.Library/Model/Payment.cs
+++ FraudDetector.Library/Model/Payment.cs
@@ -1,6 +1,6 @@
 ﻿namespace FraudDetector.Library.Model
 {
-    public record Payment
+    public record struct Payment
     {
         public float Amount { get; init; }
         public Currency Currency { get; init; }
